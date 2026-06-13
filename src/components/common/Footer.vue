0a1dd5ae0febffb40c4de55c18aa0e40e056e260
lgale <lgale@PCGALEAS>
2026-06-13 10:36:48 -0600

    feat: carpetas de componentes creada, con sub carpetas de assets, badges, logos, admin, common, game y archivos de nav, footer, button, badge ya creados con logica de codigo integrada

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="bg-dark text-white text-center py-3 mt-auto">
+    <div class="container">
+      <p class="mb-0 fs-6">&copy; 2026 - Proyecto HDP - Sistema de Aprendizaje Infantil</p>
+    </div>
+  </footer>
+</template>
